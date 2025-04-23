5c37af4b20582ee08c5f7e3b946fdfb1f0250f48
yAlex78 <85961347+yAlex78@users.noreply.github.com>
2025-04-23 15:54:44 -0700

    Created .jsx and .scss files

---
+++ app/(pages)/_components/ConferenceCarousel/ConferenceCarousel.jsx
@@ -0,0 +1,11 @@
+import styles from "./ConferenceCarousel.module.scss";
+
+export default function ConferenceCarousel()
+{
+    return 
+    (
+        <div>
+            Blank
+        </div>
+    );
+}
