561a00dedd5bf304c750037151917d2687d7b781
DiegoRAlfaro06 <Diegoramonalfarof@gmail.com>
2025-04-24 16:32:15 -0700

    Set up routes for alumni page

---
+++ app/(pages)/_components/AlumniPageCard/AlumniPageCard.jsx
@@ -0,0 +1,9 @@
+import styles from "@/app/_components/AlumniPageCard/AlumniPageCard.module.scss";
+
+export default function AlumniPageCard(){
+    return(
+        <div classname={styles.alumniPageCard}>
+            <h1> gurt: yo </h1>
+        </div>
+    )
+}
---
+++ app/(pages)/alumni/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss";
+import AlumniPageCard from "../_components/AlumniPageCard"
+
+
+export default function almuni() {
+    return(
+        <main>
+            <div> alumni </div>
+        </main>
+    );
+}
