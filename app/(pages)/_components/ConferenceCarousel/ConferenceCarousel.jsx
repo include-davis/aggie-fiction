509850b9f7f15361220abd64602efe1e4e234e18
yAlex78 <85961347+yAlex78@users.noreply.github.com>
2025-04-23 23:11:23 -0700

    Added test images

--- app/(pages)/_components/ConferenceCarousel/ConferenceCarousel.jsx
+++ app/(pages)/_components/ConferenceCarousel/ConferenceCarousel.jsx
@@ -1,5 +1,15 @@
 import styles from "./ConferenceCarousel.module.scss";
 
+const testImages = [
+    "./testImages/img1.png",
+    "./testImages/img2.png",
+    "./testImages/img3.png",
+    "./testImages/img4.png",
+    "./testImages/img5.png",
+    "./testImages/img6.png",
+    "./testImages/img7.png",
+];
+
 export default function ConferenceCarousel()
 {
     return 
