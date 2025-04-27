d4869cb9092f9fe5215005cd3d14d469e50bd01d
Theresadinh <thexresa04@gmail.com>
2025-04-26 23:53:17 -0700

    Fixed alignment + Added more features

--- app/(pages)/_components/StoreListing/StoreListing.jsx
+++ app/(pages)/_components/StoreListing/StoreListing.jsx
@@ -1,5 +1,6 @@
 import styles from "./StoreListing.module.scss";
 import React from 'react';
+import Image from 'next/image'
 
 
 export default function storelisting() {
@@ -9,33 +10,56 @@ export default function storelisting() {
           <h1 className={styles.title}>Merch Store</h1>
 
           <div className={styles.buttons}>
-            <button className={styles.btn1}>All</button>            <button className={styles.btn2}>Tote Bags</button>
+            <button className={styles.btn1}>All</button>            
+            <button className={styles.btn2}>Tote Bags</button>
             <button className={styles.btn2}>Stickers</button>
           </div>
   
         </section>
 
+          <div className={styles.cart}>
+            <button className={styles.btn4}>Cart</button>
+          </div>
+
+
         <section className={styles.listbox}>
             <h1 className={styles.listingrow1}>
               <div className={styles.card}>
                 <div className={styles.container}>
-                  {/* <img src={logo} alt="Logo" /> */}
+                  {/* <Image
+                  src="/example.png"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  /> */}
                 </div>
-                <p>Jackie Tote Bag (2nd Edition; Spring Quarter 2024)</p>
+
+                <div className={styles.productdetail}>
+                  <p className={styles.productname}>Jackie Tote Bag (2nd Edition; Spring Quarter 2024)</p>
+                  <p className={styles.cost}> $16.00</p>
+                </div>
+
               </div>
 
               <div className={styles.card}>
                 <div className={styles.container}>
         
                 </div>
-                <p>Jackie Tote Bag (2nd Edition; Spring Quarter 2024)</p>
+
+                <div className={styles.productdetail}>
+                  <p className={styles.productname}>Jackie Tote Bag (2nd Edition; Spring Quarter 2024)</p>
+                  <p className={styles.cost}> $16.00</p>
+                </div>
               </div>
 
               <div className={styles.card}>
                 <div className={styles.container}>
                 
                 </div>
-                <p>Jackie Tote Bag (2nd Edition; Spring Quarter 2024)</p>
+                
+                <div className={styles.productdetail}>
+                  <p className={styles.productname}>Jackie Tote Bag (2nd Edition; Spring Quarter 2024)</p>
+                  <p className={styles.cost}> $16.00</p>
+                </div>
+
               </div>
             </h1>
 
@@ -44,27 +68,49 @@ export default function storelisting() {
                 <div className={styles.container}>
                 
                 </div>
-                <p>Colonizer Jackie Sticker</p>
+
+                <div className={styles.productdetail}>
+                  <p className={styles.productname}> Colonizer Jackie Sticker</p>
+                  <p className={styles.cost}> $16.00</p>
+                </div>
               </div>
 
               <div className={styles.card}>
                 <div className={styles.container}>
                 
                 </div>
-                <p>Coquette Jackie Sticker</p>
+                
+                <div className={styles.productdetail}>
+                  <p className={styles.productname}> Coquette Jackie Sticker</p>
+                  <p className={styles.cost}> $16.00</p>
+                </div>
               </div>
 
               <div className={styles.card}>
                 <div className={styles.container}>
                 
                 </div>
-                <p>Boba Jackie Sticker</p>
+                
+                <div className={styles.productdetail}>
+                  <p className={styles.productname}>Boba Jackie Sticker</p>
+                  <p className={styles.cost}> $16.00</p>
+                </div>
               </div>
             </h1>
         </section> 
 
         <section className={styles.bottom}>
-          <p>1 2 3 4 5 6 7</p>
+        <div className={styles.pagination}>
+          <a href="#" class="page-link" data-page="2">&lt;&lt;</a>
+          <a href="#" class="page-link" data-page="2">&lt;</a>
+            <a href="#" class="page-link" data-page="1">1</a>
+            <a href="#" class="page-link" data-page="2">2</a>
+            <a href="#" class="page-link" data-page="3">3</a>
+            <a href="#" class="page-link" data-page="4">4</a>
+            <a href="#" class="page-link" data-page="5">5</a>
+          <a href="#" class="page-link" data-page="2">&gt;</a>
+          <a href="#" class="page-link" data-page="2">&gt;&gt;</a>
+        </div>
         </section>
       </main>
     );
