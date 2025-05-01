265e6a61837f277383aac143b8b1f33cbfa2ad5a
jasleenG <81399113+jasleenG@users.noreply.github.com>
2025-05-01 15:24:56 -0700

    made folder for blog page

---
+++ app/(pages)/blog/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss";
+import Image from 'next/image';
+
+export default function About() {
+    return (
+      <main>
+        <h1>Title</h1>
+        <p1>Description</p1> 
+      </main>
+    );
+  }
