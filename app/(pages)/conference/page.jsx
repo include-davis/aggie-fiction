43456f2025dde36288464795742f0f7b0914b24c
yAlex78 <85961347+yAlex78@users.noreply.github.com>
2025-05-01 19:57:49 -0700

    Restructured HTML components

--- app/(pages)/conference/page.jsx
+++ app/(pages)/conference/page.jsx
@@ -7,19 +7,24 @@ export default function Conference()
 {
     return (
         <main className = {styles.page}>
-            <div className = {styles.titleBlock}>
-                <div className = {styles.titleLeftBlock}>
-                    <h1>San Francisco Writers Conference</h1>
-                    <h2>A Gateway to the Literary World</h2>
+            <div className = {styles.upperPage}>
+                <div className = {styles.titleBlock}>
+                    <div className = {styles.titleLeftBlock}>
+                        <h1>San Francisco Writers Conference</h1>
+                        <h2>A Gateway to the Literary World</h2>
+                    </div>
+                    <div className = {styles.titleImageContainer}>
+                        <Image
+                            src = {titleImagePath}
+                            alt = {"Title Image"}
+                            fill
+                            sizes = "49%"
+                            className = {styles.titleImage}
+                        />
+                    </div>
                 </div>
-                <div className = {styles.titleImageContainer}>
-                    <Image
-                        src = {titleImagePath}
-                        alt = {"Title Image"}
-                        fill
-                        sizes = "50%"
-                        className = {styles.titleImage}
-                    />
+                <div className = {styles.summaryBlock}>
+                    
                 </div>
             </div>
         </main>
