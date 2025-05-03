97d7b31328dc719b686c1165a56ff6c38e3065c0
Vivian3418 <vivianzhang3418@gmail.com>
2025-05-03 08:18:25 -0700

    complete header

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,19 +1,27 @@
 import styles from "./Header.module.scss";
 import Link from 'next/link'
+import Image from 'next/image'
 export default function Header() {
     return (
         <ul className={styles.navbar}>
+            <li><Image
+      src="/logo.png"
+      width={110}
+      height={70}
+      alt="Aggie fiction logo"
+    /></li>
+            <li><Link href="/about us">About us</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
+            <li><Link href="/alumni">Alumni Spotlight</Link></li>
+            <li><Link href="/store">Merch Store</Link></li>
             <li className={styles.dropdown}>
-                <Link href="//events">Events</Link>
+                <Link href="//events"><i className={styles.arrowdown}> </i> </Link>
                 <div className={styles.dropdown_contents}>
                     <Link href="/calendar">Calendar</Link>
                     <Link href="/conference">Conference</Link>
                 </div>
             </li>
-            <li><Link href="/store">Merch Store</Link></li>
-            <li><Link href="/alumni">Alumni Spotlight</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/about us">About us</Link></li>
+
         </ul>
     );
 }
