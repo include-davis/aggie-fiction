1c961c43b368f1c4e41a84a5ff6f67694a19634a
yAlex78 <85961347+yAlex78@users.noreply.github.com>
2025-05-02 17:56:47 -0700

    Finished adding all the components of the conference page

--- app/(pages)/conference/page.jsx
+++ app/(pages)/conference/page.jsx
@@ -2,6 +2,8 @@ import styles from "./page.module.scss";
 import Image from "next/image";
 
 const titleImagePath = "/conferencePageImages/titleImage.png";
+const summaryImagePath = "/conferencePageImages/summaryImage.png";
+const involvementImagePath = "/conferencePageImages/involvementImage.png";
 
 export default function Conference() 
 {
@@ -19,12 +21,49 @@ export default function Conference()
                             alt = {"Title Image"}
                             fill
                             sizes = "49%"
-                            className = {styles.titleImage}
+                            className = {styles.conferenceImage}
                         />
                     </div>
                 </div>
                 <div className = {styles.summaryBlock}>
-                    
+                    <div className = {styles.summaryImageContainer}>
+                        <Image
+                            src = {summaryImagePath}
+                            alt = {"Summary Image"}
+                            fill
+                            sizes = "34%"
+                            className = {styles.conferenceImage}
+                        />
+                    </div>
+                    <div className = {styles.summaryRightBlock}>
+                        <p>The San Francisco Writers Conference (SFWC) is an annual four-day event that brings together writers, editors, literary agents, publishers, and industry professionals from around the globe. Held at the iconic Hyatt Regency, the conference offers a packed schedule of workshops, panels, and networking opportunities. Whether you're an aspiring novelist, a seasoned writer, or simply curious about the publishing world, SFWC provides a rare chance to gain industry insights, hone your craft, and make meaningful connections.</p>
+                        <p>The event features sessions on everything from world-building and character development to query letters, marketing strategies, and self-publishing tips. It’s not just about learning the business side of writing—it’s also a celebration of creativity, where authors share their journeys and inspire others to pursue their literary dreams.</p>
+                    </div>
+                </div>
+            </div>
+
+            <h1>CAROUSEL GOES HERE</h1>
+
+            <div className = {styles.lowerPage}>
+                <div className = {styles.lowerLeftBlock}>
+                    <div className = {styles.involvementBlock}>
+                        <h1>Aggie Fiction's Involvement</h1>
+                        <p>Every year, our club volunteers at the conference, helping with event coordination, guest services, and logistical support. This hands-on role gives us behind-the-scenes access to the event while allowing us to <b>engage directly with industry professionals.</b> It’s an incredible way to <b>build connections</b> and <b>gain insights</b> that go beyond the typical attendee experience.</p>
+                        <p>For our club members, SFWC isn’t just a conference—<b>it’s an opportunity to represent UC Davis</b>, network with literary professionals, and bring back insights and experiences that strengthen our creative writing community.</p>
+                    </div>
+                    <div className = {styles.involvementBlock}>
+                        <h2>Student Ambassador</h2>
+                        <p>Aggie Fiction proudly serves as a student ambassador for SFWC. The conference hosts a Student Forum, a space dedicated to young and emerging writers, and we’ve taken the initiative to invite and connect with students from other universities. By expanding the forum’s reach, we help foster a larger community of student creatives and encourage more young writers to participate in this invaluable industry event.</p>
+                    </div>
+                </div>
+                <div className = {styles.involvementImageContainer}>
+                    <Image
+                        src = {involvementImagePath}
+                        alt = {"Involvement Image"}
+                        fill
+                        sizes = "43%"
+                        className = {styles.conferenceImage}
+                    />
                 </div>
             </div>
         </main>
