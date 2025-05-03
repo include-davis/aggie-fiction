bfae7039bd07e3cb5d94dc0b318351bcc9996697
jasleenG <81399113+jasleenG@users.noreply.github.com>
2025-05-02 17:39:36 -0700

    trying to add image

--- app/(pages)/blog/page.jsx
+++ app/(pages)/blog/page.jsx
@@ -4,6 +4,12 @@ import Image from 'next/image';
 export default function About() {
     return (
       <main>
+        <Image
+        src= "public/HomeCarousel/img/img2.png"
+        width={1312}
+        height={400}
+        alt= "notebook"
+        />
         <h1>Title</h1>
         <p1>Description</p1> 
       </main>
