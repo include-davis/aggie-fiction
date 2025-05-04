cfe9360dc27b7405f9e43ba27d3af3781e2c136d
Vivian3418 <vivianzhang3418@gmail.com>
2025-05-04 00:09:07 -0700

    add logo

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,27 +1,28 @@
 import styles from "./Header.module.scss";
 import Link from 'next/link'
 import Image from 'next/image'
+import Script from 'next/script'
 export default function Header() {
     return (
         <ul className={styles.navbar}>
-            <li><Image
-      src="/logo.png"
-      width={110}
-      height={70}
-      alt="Aggie fiction logo"
-    /></li>
             <li><Link href="/about us">About us</Link></li>
             <li><Link href="/blog">Blog</Link></li>
             <li><Link href="/alumni">Alumni Spotlight</Link></li>
             <li><Link href="/store">Merch Store</Link></li>
             <li className={styles.dropdown}>
-                <Link href="//events"><i className={styles.arrowdown}> </i> </Link>
+                <p className={styles.Events}>Events</p> <Link href="//events"><i className={styles.arrowdown}></i> </Link>
                 <div className={styles.dropdown_contents}>
                     <Link href="/calendar">Calendar</Link>
                     <Link href="/conference">Conference</Link>
                 </div>
             </li>
-
+            <li className={styles.logo}><Link href=""> <Image
+                src="/logo.png"
+                width={110}
+                height={70}
+                alt="Aggie fiction logo"
+            /></Link></li>
         </ul>
+
     );
 }
