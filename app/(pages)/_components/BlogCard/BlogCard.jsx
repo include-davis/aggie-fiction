0a129b71d8c858c586ea5e28d2cab813363cd63d
jasleenG <81399113+jasleenG@users.noreply.github.com>
2025-05-05 18:12:23 -0700

    setting up blog page + components

---
+++ app/(pages)/_components/BlogCard/BlogCard.jsx
@@ -0,0 +1,7 @@
+import styles from './BlogCard.module.scss'; 
+
+export default function BlogCard ({ imageSrc, title, description}){
+    return(
+        <div></div>
+    );
+}
---
+++ app/(pages)/blog/blog.jsx
@@ -0,0 +1,10 @@
+
+import styles from "./blog.module.scss"; 
+import Image from "next/image";
+
+
+export default function About() {
+    return (
+      <main></main>
+    );
+  }
