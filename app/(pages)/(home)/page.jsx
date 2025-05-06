c52f61f1d12f05cb1194760cbb4c3baf683f7cee
Naomi Zhao <naomitan.zhao@gmail.com>
2025-05-05 20:10:44 -0700

    Restore change to homepage

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -2,11 +2,9 @@ import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <>
     <main className={styles.page}>
       <h1>header 1</h1>
       <p>body</p>
     </main>
-    </>
   );
 }
