4b87b4ebd1c6f6520dbf19ba7aa41b163e5c5265
Naomi Zhao <naomitan.zhao@gmail.com>
2025-05-07 10:09:12 -0700

    Add onClick handler to button and make onClick and route optional

--- app/(pages)/_components/Button/Button.jsx
+++ app/(pages)/_components/Button/Button.jsx
@@ -1,18 +1,21 @@
+// "use client";
 import styles from "./Button.module.scss";
 import Link from 'next/link'
 import React from 'react';
 
 /**
  * color: either "light", "dark", or "gradient", defaults to light
- * route: page route to link to, such as /about
+ * route: page route to link to, such as /about. default is no route
+ * onClick: onClick handler, you can pass any function in here. defaut is no function
  * width: optional prop specifying the button width in pixels, defaults to 272
  */
+
 /**
  * example usage: <Button route="/about" color="dark" width={100}>
  *                    Button Text Here
                   </Button>
  *  */ 
-export default function Button({children, color="light", route, width=272}) {
+export default function Button({children, onClick=null, color="light", route="", width=272}) {
     let colorClass;
     if (color==="light") {
         colorClass=styles.lightButton;
@@ -26,7 +29,11 @@ export default function Button({children, color="light", route, width=272}) {
     return (
         <div>
                 <Link href={route}>
-                    <button className={`${styles.Button} ${colorClass}`} style={{width: `${width}px`}}>
+                    <button 
+                        onClick={onClick}
+                        className={`${styles.Button} ${colorClass}`} 
+                        style={{width: `${width}px`}}
+                    >
                         {children}
                     </button>
                 </Link>
