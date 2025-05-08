766ee4d0786f40f5df70e26c723298217ae81015
Vivian3418 <vivianzhang3418@gmail.com>
2025-05-08 12:35:05 -0700

    make dropdown clickable

--- app/(pages)/_components/Header/Header.jsx
+++ app/(pages)/_components/Header/Header.jsx
@@ -1,28 +1,41 @@
+'use client'
 import styles from "./Header.module.scss";
 import Link from 'next/link'
 import Image from 'next/image'
-import Script from 'next/script'
+import { useState } from 'react'
 export default function Header() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggleDropdown = () => {
+        setDropdownOpen(prev => !prev);
+};
     return (
-        <ul className={styles.navbar}>
-            <li><Link href="/about us">About us</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/alumni">Alumni Spotlight</Link></li>
-            <li><Link href="/store">Merch Store</Link></li>
-            <li className={styles.dropdown}>
-                <p className={styles.Events}>Events</p> <Link href="//events"><i className={styles.arrowdown}></i> </Link>
-                <div className={styles.dropdown_contents}>
-                    <Link href="/calendar">Calendar</Link>
-                    <Link href="/conference">Conference</Link>
-                </div>
-            </li>
-            <li className={styles.logo}><Link href=""> <Image
+        <div className={styles.navbar}>
+            <div className={styles.logo}><Link href=""> <Image
                 src="/logo.png"
                 width={110}
                 height={70}
                 alt="Aggie fiction logo"
-            /></Link></li>
-        </ul>
-
-    );
+            /></Link></div>
+            <div className={styles.navContents}>
+                <div className={styles.dropdown}>
+                    <Link href="//events" >Events</Link> <button className={styles.arrow} onClick={toggleDropdown} ><Image
+                    src="/weui_arrow-outlined.svg"
+                    width={12}
+                    height={24}
+                    alt="Picture of the arrow"
+                />
+                </button>
+                </div>
+                {dropdownOpen &&(
+                <div className={styles.dropdown_contents}>
+                    <Link href="/calendar">Calendar</Link>
+                    <Link href="/conference">Conference</Link>
+                </div>)}
+                <Link href="/store">Merch Store</Link>
+                <Link href="/alumni">Alumni Spotlight</Link>
+                <Link href="/blog">Blog</Link>
+                <Link href="/about us">About us</Link>
+            </div>
+        </div>
+            );
 }
