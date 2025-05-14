6224b36800029980fcba2f3287cba68a92786b50
Vivian3418 <vivianzhang3418@gmail.com>
2025-05-13 17:20:51 -0700

    finish blog page

--- app/(pages)/blog/[id]/page.jsx
+++ app/(pages)/blog/[id]/page.jsx
@@ -22,20 +22,23 @@ export default function Page() {
                 <Image
                     src="/blog image.jpg"
                     alt="image"
-                    width={1312}
-                    height={400}/>
+                    style={{objectFit:"cover"}}
+                    fill={true}
+                    />
             </div>
         <div className={styles.header}>
             <h1>Ten Mistakes Amateur Writers Make</h1>
         </div>
-        <div className={styles.subheader}
-             style={{alignItems:"flex-start"}}>
+        <div className={styles.subheader}>
             <p style={{fontWeight: "bold"}}>subheader:</p>
             <p>content</p>
         </div>
         <div className={styles.blogcontent}>
             <p className={styles.lineseparator}></p>
-            <p className={styles.content}>content</p>
+            <div className={styles.content}>
+            <p >You have a six hour movie playing in your mind, an open document with approximately zero to a bajillion words and a little question niggling at the back of your mind.
+                Is any of this actually good? </p>
+                </div>
             <div className={styles.category}>
                 <p>Categories:</p>
                 <Button route="/writing style" color="light">
@@ -47,16 +50,20 @@ export default function Page() {
             <div className={styles.AboutAuthor}>
                 <h2>About the Author</h2>
                 <div className={styles.AuthorContent}>
+                    <div className={styles.author_image}>
                     <Image
                         src="/Author_image.jpg"
                         alt="image of author"
-                        width={372}
-                        height={372} />
+                        style={{objectFit:"cover"}}
+                        fill={true}
+                         />
+                        </div>
                     <div className={styles.AuthorInfor}>
                     <h2>Author Name</h2>
                     <p>introduction</p>
                         </div>
                 </div>
+            </div>
             </div>
             <div className={styles.EndButtons}>
                 <div className={styles.button}>
@@ -70,7 +77,6 @@ export default function Page() {
                         alt="picture of the arrow"/></Link>
                 </button>
             </div>
-        </div>
     </div>
 
 
