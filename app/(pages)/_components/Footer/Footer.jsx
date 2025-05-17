bcdfbd9b3cd43006d5035b7e7e2d78185e91d628
Vivian3418 <vivianzhang3418@gmail.com>
2025-05-17 16:50:53 -0700

    revise the footer

--- app/(pages)/_components/Footer/Footer.jsx
+++ app/(pages)/_components/Footer/Footer.jsx
@@ -2,23 +2,31 @@ import styles from "./Footer.module.scss";
 import Link from 'next/link'
 import Image from 'next/image'
 import { FaDiscord } from "react-icons/fa";
-import { CiInstagram } from "react-icons/ci";
 import { MdEmail } from "react-icons/md";
 export default function Footer() {
     return (
       <div className={styles.Footer}>
+          <div className={styles.logo}>
          <Link href=""> <Image
       src="/logo.png"
       width={110}
       height={70}
       alt="logo"
          /></Link>
+              </div>
           <div className={styles.Footer_icons}>
        <p className={styles.Footer_text}>Follow Our Socials</p>
               <div className={styles.icons}>
-                  <MdEmail className={styles.icon} />
-                  <FaDiscord className={styles.icon} />
-                  <CiInstagram className={styles.icon} /></div>
+                  <a href="#"><MdEmail className={styles.icon} /></a>
+                  <a href="#"><FaDiscord className={styles.icon} /></a>
+                  <a href="#">
+                      <Image
+                          src="/lets-icons_insta.svg"
+                          width={39}
+                          height={39}
+                          alt="inst icon"/>
+                  </a>
+                  </div>
       </div>
 
 
