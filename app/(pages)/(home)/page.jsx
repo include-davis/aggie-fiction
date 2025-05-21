5c3fa67388f39236aeeca2a6774b096fbae14068
Kelly Choi <kelchoi@ucdavis.edu>
2025-05-21 15:26:44 -0700

    edits

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,11 +1,28 @@
 import styles from "./page.module.scss";
 import HomeCarousel from "../_components/HomeCarousel/HomeCarousel";
+import { ImageResponse } from "next/server";
 
 export default function Home() {
   return (
     <main className={styles.page}>
-      <h1>header 1</h1>
-      <p>body</p>
+      <h1>Blog</h1>
+      className={styles.categories}>
+      <Image
+        src='/blogMainImages/otter.png'
+        alt='Otter Dialogue'
+        width={252}
+        height={265}
+      />
+      <h2>Categories</h2>
+      <Image
+        src='/blogMainImages/line.png'
+        alt='Line'
+        width={180}
+        height={0}
+      />
+      <HomeCarousel
+      //use button component
+
       <HomeCarousel />
     </main>
   );
