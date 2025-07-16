d3975a9fae15ab6e6c0dd8cd885feebbef2680b2
Naomi Zhao <naomitan.zhao@gmail.com>
2025-07-15 21:10:06 -0700

    refactor: Separate fallback data

--- app/(pages)/alumni/page.jsx
+++ app/(pages)/alumni/page.jsx
@@ -1,107 +1,5 @@
 import AlumniPageContent from "../_components/AlumniPageContent/AlumniPageContent.jsx"
-
-const alumniList = [
-  {
-    first_name:"Jack",
-    last_name:"Hyslop",
-    role1:"Founder, 2020–2022",
-    role2:"",
-    role3:"",
-    description:(
-    <>
-      Jack started the club with his friends Maeve and Isabella in Winter Quarter 2020.
-      He remained as primary leader until his graduation in Spring Quarter 2022.
-      <br />
-      <br />
-      Since graduating, he has stayed creatively engaged.
-      Recently, he has become a drummer in a band with his friends.
-      He has taken sewing classes at a local city college and joined Radical Sewing Club (@radicalsewingclub) as a volunteer assisting folks
-      with using a sewing machine to mend and alter their clothes.
-      He rescued a kitten named Boots about two years ago.
-      </>
-    ),
-    imageUrl:"/AlumniPage/jack.png",
-    instaHandle:"@radicalsewingclub",
-    instaLink:"https://www.instagram.com/", /*General link for now*/
-    linkedinLink:"https://www.linkedin.com/", /*General link for now*/
-    linkedinHandle:"linkedin link",
-  },
-   {
-    first_name:"Leila",
-    last_name:"Sanchez",
-    role1:"President, 2020–2023",
-    description:(
-    <>
-      After graduation in 2023, Leila Sanchez now works at the Social Security Administration 
-      while working on her writing career in Urban Fantasy and Horror. 
-      She self published the short story, The Challenge at the Carmichael Mansion in late 2023, 
-      and is currently on other writing projects, while breaking down tropes and providing her own 
-      analysis on shows and movies on her TikTok and Instagram, @Leilasstorycorner.
-      </>
-    ),
-    imageUrl:"/AlumniPage/Leila.jpg",
-    instaHandle:"@Leilasstorycorner",
-    instaLink:"https://www.instagram.com/", /*General link for now*/
-    linkedinLink:"https://www.linkedin.com/", /*General link for now*/
-    linkedinHandle:"linkedin link",
-  },
-   {
-    first_name:"Annie",
-    last_name:"Tran",
-    role1:"Vice President, 2022–2023",
-    role2:"President, 2023-2024",
-    role3:"",
-    description:(
-    <>
-    Annie Tran joined the club in fall of 2020—during lockdown—serving as president in her final year at davis. 
-    Now having returned home, she is working in quality assurance and, in her leisure time, writing a fantasy novel.
-      </>
-    ),
-    imageUrl:"/AlumniPage/Annie.jpg",
-    instaHandle:"@instagram",
-    instaLink:"https://www.instagram.com/", /*General link for now*/
-    linkedinLink:"https://www.linkedin.com/", /*General link for now*/
-    linkedinHandle:"linkedin link",
-  },
-   {
-    first_name:"Jakob",
-    last_name:"Stanton",
-    role1:"Vice President, 2023–2024",
-    description:(
-    <>
-      Jakob Stanton was the secondary leader of Aggie Fiction from 2023-2024. 
-      He graduated 2024 with a bachelor&apos;s degree in History and a minor in sociology. 
-      He researched and wrote an honors thesis about aeronautical culture in Germany from 1872-1929 titled We Dead Fliers. 
-      He is currently pursuing a masters in Screenwriting at Chapman University in Orange CA where he is trying to solve the 
-      puzzle that is screenwriting with two sweet black cats named Bucket and Boba.
-      </>
-    ),
-    imageUrl:"/AlumniPage/Jakob.png",
-    instaHandle:"@instagram",
-    instaLink:"https://www.instagram.com/", /*General link for now*/
-    linkedinLink:"https://www.linkedin.com/", /*General link for now*/
-    linkedinHandle:"linkedin link",
-  },
-   {
-    first_name:"Erika",
-    last_name:"DiMaano",
-    role1:"Publicity Chair, 2022–2024",
-    role2:"",
-    role3:"",
-    description:(
-    <>
-      Erika DiMaano is an interaction designer who&apos;s been working on various interactive digital projects and design work. 
-      She&apos;s recently been branching out to web development and learning new skills to bring her work further.
-      </>
-    ),
-    imageUrl:"/AlumniPage/Erika.png",
-    instaHandle:"@instagram",
-    instaLink:"https://www.instagram.com/", /*General link for now*/
-    linkedinLink:"https://www.linkedin.com/", /*General link for now*/
-    linkedinHandle:"linkedin link",
-  },
-
-]
+import AlumniFallbackData from "../../_data/alumni-cards.json"
 
 async function getCards() {
   console.log("start get cards");
@@ -135,7 +33,7 @@ async function getCards() {
     return parsedData;
   } catch (e) {
     console.error(`Failed to fetch alumni-cards: ${e.message}`);
-    return alumniList;
+    return AlumniFallbackData;
   }
 }
 
