b3cdd005c4b988a39084c75fae059740ee9d95e3
Naomi Zhao <naomitan.zhao@gmail.com>
2025-10-11 20:54:06 -0700

    fix: Board Member component fixxes

--- app/(pages)/_components/BoardMembers/BoardMembers.jsx
+++ app/(pages)/_components/BoardMembers/BoardMembers.jsx
@@ -1,19 +1,15 @@
 /* eslint-disable react/prop-types */
-'use client'
 import styles from "./BoardMembers.module.scss"
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 
 export default function BoardMembers({ board }) {
-   /* eslint-disable-next-line no-unused-vars */
-    const [boardMembers, setBoardMembers] = useState(board);
-    console.log(boardMembers);
     return (
         <div className={styles.board}>
           <h1>Our board</h1>
           <div className={styles.boardContent}>
             <div className={styles.boardGrid}>
-              {boardMembers.map((member, i) => (
+              {board.map((member, i) => (
                 <div className={styles.member} key={i}>
                   <div className={styles.memberImageContainer}>
                     <Image
@@ -27,7 +23,7 @@ export default function BoardMembers({ board }) {
                     <p className={styles.position}>{member.role}</p>
                     <p>{member.majors.length > 1 ? "Majors: " + member.majors.join(" & ") : "Major: " + member.majors[0]}</p>
                     {member.minors.length > 0 && <p>{member.minors.length > 1 ? "Minors: " + member.minors.join(" & ") : "Minor: " + member.minors[0]}</p>}
-                    <p>{member.year}</p>
+                    <p>{member.grade}</p>
                   </div>
                 </div>
               ))}
