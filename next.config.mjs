3d59c868028500182093f5fde55bd3037c484211
Naomi Zhao <naomitan.zhao@gmail.com>
2025-10-11 17:07:56 -0700

    fix: Modify next.config.mjs to work in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === "development";
+
 const cspHeader = `
   default-src 'self';
   connect-src 'self' https://*.googleapis.com https://cms.aggiefiction.com;
-  `;
+  img-src 'self' https://res.cloudinary.com data:;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  ${isDev
+    ? "script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+    : "script-src 'self';"}
+`;
 
 const nextConfig = {
   reactStrictMode: true,
