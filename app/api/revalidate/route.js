82aa7847edfa3aa701c7dcb031d61eaf416178bb
Naomi Zhao <naomitan.zhao@gmail.com>
2025-10-11 18:36:33 -0700

    feat: Update revalidation

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,6 +1,9 @@
+export const runtime = "nodejs";
+
 import { revalidateTag } from "next/cache";
 
 export async function GET() {
+  console.log('revalidate');
   revalidateTag("cms");
   return Response.json({ revalidated: true });
-}
+};
