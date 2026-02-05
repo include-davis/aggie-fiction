8e32dfae8018633007a26b278b918d1713f8c28e
aggiefiction <aggiefictionucd@gmail.com>
2026-02-04 18:39:45 -0800

    Update Footer.jsx
    Added *corrected* links to Instagram, Discord and email for footer. Email link should be working now.

--- app/(pages)/_components/Footer/Footer.jsx
+++ app/(pages)/_components/Footer/Footer.jsx
@@ -21,13 +21,13 @@ export default function Footer() {
             <div className={styles.footer_content}>
                 <p className={styles.footer_text}>Follow Our Socials</p>
                 <div className={styles.icons}>
-                    <a href="#">
+                    <a href="mailto:aggiefictionucd@gmail.com">
                         <MdEmail className={styles.icon} />
                     </a>
-                    <a href="#">
+                    <a href="https://discord.gg/bBTTbxcjnk">
                         <FaDiscord className={styles.icon} />
                     </a>
-                    <a href="#">
+                    <a href="https://www.instagram.com/aggiefiction">
                         <div className={styles.icon}>
                             <Image
                                 src="/lets-icons_insta.svg"
