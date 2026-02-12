4673259f57e042c3ab9cc86e9eb07918ef7623f1
Naomi Zhao <naomitan.zhao@gmail.com>
2026-02-11 23:39:46 -0800

    feat: Add contact links

--- app/(pages)/_components/Footer/Footer.jsx
+++ app/(pages)/_components/Footer/Footer.jsx
@@ -5,7 +5,27 @@ import { FaDiscord } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import React from "react";
 
-export default function Footer() {
+async function getJoinLink() {
+  try {
+    const res = await fetch(
+      // eslint-disable-next-line no-undef
+      `${process.env.NEXT_PUBLIC_CMS_BASE_URL}/api/content/general-info?_published=true`,
+      { cache: "no-store" }
+    );
+    const data = await res.json();
+    if (!data.ok || !data.body || data.body.length === 0) {
+      throw new Error(data.error);
+    }
+    const parsedData = data.body[0].join_link;
+    return parsedData;
+  } catch (e) {
+    console.error(`Failed to fetch join link: ${e.message}`);
+    return "/";
+  }
+}
+
+export default async function Footer() {
+    const joinLink = await getJoinLink();
     return (
       <div className={styles.footer}>
           <div className={styles.logo}>
@@ -21,13 +41,13 @@ export default function Footer() {
             <div className={styles.footer_content}>
                 <p className={styles.footer_text}>Follow Our Socials</p>
                 <div className={styles.icons}>
-                    <a href="#">
+                    <a href="mailto:aggiefictionucd@gmail.com">
                         <MdEmail className={styles.icon} />
                     </a>
-                    <a href="#">
+                    <a href={joinLink}>
                         <FaDiscord className={styles.icon} />
                     </a>
-                    <a href="#">
+                    <a href="https://www.instagram.com/aggiefiction">
                         <div className={styles.icon}>
                             <Image
                                 src="/lets-icons_insta.svg"
